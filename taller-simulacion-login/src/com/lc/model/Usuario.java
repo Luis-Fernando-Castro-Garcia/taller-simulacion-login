31c38aa75cc7be8fa095264fcdf4af647a0d7ae8
Luis Castro <lcastro-2025499@kinal.edu.gt>
2026-07-14 07:52:23 -0600

    Se agregaron los paquetes MVC, Styles, y las primeras clases.

---
+++ taller-simulacion-login/src/com/lc/model/Usuario.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.lc.model;
+
+public class Usuario {
+    private String nombre_usuario;
+    private String password;
+    private String nombreCompleto;
+    private Rol rol;
+}
